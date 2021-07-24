6a80f68b0ad7072b91de95ed02baf6d34d10b6f2
Clarenti Celsia <66846357+clarentclaire@users.noreply.github.com>
2021-07-24 17:16:10 +0700

    Update Human.kt

--- app/src/main/java/com/example/solid/L/After/Human.kt
+++ app/src/main/java/com/example/solid/L/After/Human.kt
@@ -1,10 +1,13 @@
 package com.example.solid.L.After
 
 open class Human {
+    fun walk(){}
+}
+
+open class NonDisability: Human(){
     fun see(){}
 }
 
-open class Blind: Human(){
-    fun walk(){}
-}
+class BlindGroup: Human()
+class Normal: NonDisability() 
 
